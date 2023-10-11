9d92ce71be7e5aca04577a7541bcb4d374e83ed9
Yanwen Xu <yxu83@ucsc.edu>
2023-10-11 13:44:54 -0700

    rm

--- icp/SimpleICP.cpp
+++
@@ -1,6 +0,0 @@
-#include "SimpleICP.hpp"
-
-Eigen::MatrixXi KnnSearch(const Eigen::MatrixXd &X,
-                          const Eigen::MatrixXd &X_query, const int &k) {
-  return {};
-}
--- icp/SimpleICP.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <Eigen/Dense>
-
-Eigen::MatrixXi KnnSearch(const Eigen::MatrixXd &X,
-                          const Eigen::MatrixXd &X_query, const int &k = 1);
